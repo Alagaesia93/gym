c63e0b680231f1a06b482e572a9698a2aefc6861
Alessandro Macagno <ale.macagno@hotmail.it>
2019-12-17 17:22:58 +0100

    counter culture for total duration

--- app/models/exercise_workout.rb
+++ app/models/exercise_workout.rb
@@ -3,4 +3,10 @@
 class ExerciseWorkout < ApplicationRecord
   belongs_to :workout
   belongs_to :exercise
+
+  delegate :duration, to: :exercise
+
+  counter_culture :workout,
+                  column_name: 'total_duration',
+                  delta_column: 'duration'
 end
--- spec/factories/workouts.rb
+++ spec/factories/workouts.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :workout do
     name { Faker::Name.name }
-    total_duration { 100 }
+    total_duration { 0 }
     state { 0 }
     creator { Trainer.last || FactoryBot.create(:trainer) }
   end
