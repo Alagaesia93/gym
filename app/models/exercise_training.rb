3fce1339af8552737f4210e1a059faa63883e0ed
Alessandro Macagno <ale.macagno@hotmail.it>
2019-12-18 15:42:33 +0100

    improvements in validation

--- app/models/exercise_training.rb
+++ app/models/exercise_training.rb
@@ -3,4 +3,6 @@
 class ExerciseTraining < ApplicationRecord
   belongs_to :exercise
   belongs_to :training
+
+  validates_uniqueness_of :exercise_id, scope: %i[training_id]
 end
--- spec/models/exercise_training_spec.rb
+++ spec/models/exercise_training_spec.rb
@@ -6,4 +6,5 @@ RSpec.describe ExerciseTraining, type: :model do
   subject { build(:exercise_training) }
   it { should belong_to(:exercise) }
   it { should belong_to(:training) }
+  it { should validate_uniqueness_of(:exercise_id).scoped_to(:training_id) }
 end
